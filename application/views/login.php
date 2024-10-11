aafd53c49a05b9c68e10cf5745b47801e403daff
Dani170907 <daniramadhan170907@gmail.com>
2024-10-11 09:50:48 +0700

    configurasi

---
+++ application/views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In Page</title>
+</head>
+<body>
+    <h1>Halaman Login</h1>
+    
+    <input type="text" placeholder="masukan username" name="username"> <br><br>
+    <input type="password" placeholder="masukan password" name="password"> <br><br>
+    
+    <button type="submit" value="LOGIN">Log In
+        
+</body>
+</html>
