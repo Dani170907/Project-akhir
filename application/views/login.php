52d45f20ea6f4519f8dd7571955552e168081dec
Dani170907 <daniramadhan170907@gmail.com>
2024-10-12 10:27:40 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -6,28 +6,31 @@
     <title>Login</title>
     <link href="<?= base_url('assets/css/output.css'); ?>" rel="stylesheet"> <!-- Import file CSS yang dihasilkan -->
 </head>
-<body class="bg-gray-100 h-screen flex items-center justify-center">
-    <div class="w-full max-w-xs">
-        <form action="<?= base_url('login/verifikasi'); ?>" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+<body>
+
+    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+        <img src="https://picsum.photos/1920/1080" alt=""
+            class="w-full h-full object-cover filter blur-lg brightness-50">
+    </div>
+
+    <!-- Login Form -->
+    <div class="relative z-10 bg-white p-8 rounded-md shadow-lg">
+        <h1 class="text-xl font-bold mb-4">Login</h1>
+        <form action="<?= base_url() ?>Login/verifikasi" method="POST">
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                    Username
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username" placeholder="Username">
-            </div>
-            <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" placeholder="**********">
-            </div>
-            <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    Login
-                </button>
-                <p>Don't have account? <a href="<?= base_url() ?>Registrasi">Sign Up</a></p>
+                <label for=""></label>
             </div>
         </form>
     </div>
+    
+
+        <input type="text" placeholder="masukan username" name="username"> <br><br>
+        <input type="password" placeholder="masukan password" name="password"> <br><br>
+        
+        <button type="submit">Log In</button>
+    </form>
+    
+    <br>
+    <p>Don't have account? <a href="<?= base_url() ?>Registrasi">Sign Up</a></p>
 </body>
 </html>
