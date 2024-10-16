f764dd014070e7d94a706816ea7795464de7db5d
Dani170907 <daniramadhan170907@gmail.com>
2024-10-16 15:07:21 +0700

    menambahkan helper login untuk menolak user yang tidak memiliki session

--- application/views/event.php
+++ application/views/event.php
@@ -1,1 +1,14 @@
-<h1>Event</h1>
+<body>
+    <a href="">Tambah Data</a>
+
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Nama Event</th>
+            <th>Penyelenggata</th>
+            <th>Aksi</th>
+        </tr>
+        <th></th>
+    </table>
+    
+</body>
--- application/views/templates/admin_header.php
+++ application/views/templates/admin_header.php
@@ -49,7 +49,7 @@
 				</svg>
 			</button>
 		</div>
-		<ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+		<ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
 			<li><a class="text-sm <?= ($currentPage == 'admin/beranda') ? 'text-blue-600 font-bold' : 'text-gray-400' ?> hover:text-gray-500" href="<?= base_url('admin/beranda') ?>">Beranda</a></li>
 			<li class="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
@@ -62,7 +62,7 @@
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a class="text-sm <?= ($currentPage == 'admin/event') ? 'text-blue-600 font-bold' : 'text-gray-400' ?> hover:text-gray-500" href="<?= base_url('admin/event') ?>">Event</a></li>
+			<li><a class="text-sm <?= ($currentPage == 'admin/event') ? 'text-blue-600 font-bold' : 'text-gray-400' ?> hover:text-gray-500" href="<?= base_url('admin/event') ?>">Data Event</a></li>
 			</li>
 		</ul>
 		<a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-50 font-semibold rounded-xl transition duration-200" href="<?= base_url() ?>Login/logout">Log Out</a>
@@ -89,13 +89,13 @@
 						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href<?= base_url('admin/pendaftaran') ?>">Pendaftaran</a>
 					</li>
 					<li class="mb-1">
-						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="<?= base_url('admin/event') ?>">Event</a>
+						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="<?= base_url('admin/event') ?>">Data Event</a>
 					</li>
 				</ul>
 			</div>
 			<div class="mt-auto">
 				<div class="pt-6">
-					<a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="<?= base_url() ?>Login/logout">Log Out</a>
+					<a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl" href="<?= base_url() ?>Login/logout">Log Out</a>
 				</div>
 			</div>
 		</nav>
